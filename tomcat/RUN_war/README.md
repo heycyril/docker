a64825cf736a62f299f27dfc6f7808eef952b0d1
cyril <36559067@qq.com>
2018-08-24 04:14:44 +0800

    .

--- tomcat/RUN_war/README.md
+++ tomcat/RUN_war/README.md
@@ -1,6 +1,6 @@
 在当前目录下执行编译镜像
 
-    #cyril/jardemo 镜像名字
+    #cyril/wardemo 镜像名字
 
     #. Dockerfile文件在当前文件夹下
 
@@ -8,8 +8,9 @@
 
 等编译好后运行容器
 
-    docker run -d -p 8282:8080 -v /docker/docker/tomcat/conf:/usr/local/tomcat/conf -v /data/tomcat/logs:/opt/tomcat/logs -v /docker/docker/tomcat/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 cyril/wardemo:0.1
+docker run -d -p 8383:8080 --privileged=true -v /root/tomcat/docker/tomcat/RUN_war/conf:/usr/local/tomcat/conf -v /root/tomcat/docker/tomcat/RUN_war/logs:/opt/tomcat/logs -v /root/tomcat/docker/tomcat/RUN_war/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 gsy36559067/tomcat:8.0.1
 
+    其中：–privileged=true是授予docker挂载的权限
 查看运行状态
 
     docker ps
--- tomcat/RUN_war/run.sh
+++ tomcat/RUN_war/run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
-#docker run -d -p 8383:8080 -v /docker/docker/tomcat/conf:/usr/local/tomcat/conf -v /data/tomcat/logs:/opt/tomcat/logs -v /docker/docker/tomcat/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 gsy36559067/tomcat:8.0.1
-
 #在当前目录下执行编译镜像
-docker build -t cyril/wardemo:0.1 .
+#docker build -t cyril/wardemo:0.1 .
 
 #将打包好的war包拷贝到webapps下面
 
 #等编译好后运行容器
-docker run -d -p 8282:8080 -v /docker/docker/tomcat/conf:/usr/local/tomcat/conf -v /data/tomcat/logs:/opt/tomcat/logs -v /docker/docker/tomcat/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 cyril/wardemo:0.1
+#docker run -d -p 8282:8080 -v /docker/docker/tomcat/conf:/usr/local/tomcat/conf  -v /docker/docker/tomcat/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 docker.io/gsy36559067/tomcat:0.0.1
+
+docker run -d -p 8383:8080 --privileged=true -v /root/tomcat/docker/tomcat/RUN_war/conf:/usr/local/tomcat/conf -v /root/tomcat/docker/tomcat/RUN_war/logs:/opt/tomcat/logs -v /root/tomcat/docker/tomcat/RUN_war/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 gsy36559067/tomcat:8.0.1
+
+#docker run -d -p 8383:8080 --privileged=true -v /root/tomcat/docker/tomcat/RUN_war/conf:/usr/local/tomcat/conf -v /root/tomcat/docker/tomcat/RUN_war/logs:/opt/tomcat/logs -v /root/tomcat/docker/tomcat/RUN_war/webapps:/usr/local/tomcat/webapps --name tomcat-test-1 cyril/wardemo:0.1
 
 #查看运行状态
 docker ps
-
-
-
