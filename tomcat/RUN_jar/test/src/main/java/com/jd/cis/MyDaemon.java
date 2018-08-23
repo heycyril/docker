7945d177a18c4e4fcb4e62717ff1d03ed4e680bb
cyril <36559067@qq.com>
2018-08-23 23:18:00 +0800

    .

---
+++ tomcat/RUN_jar/test/src/main/java/com/jd/cis/MyDaemon.java
@@ -0,0 +1,18 @@
+package com.jd.cis;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mlzboy
+ * Date: 12-2-23
+ * Time: 上午12:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyDaemon {
+    public static void main(String[] args)  {
+        while(true)
+        {
+            System.out.println("dddddddddddddd");
+        }
+    }
+}
