6d468fa1475a1f22de9a4dfa9f8d86b843ace7a4
cyril <36559067@qq.com>
2018-08-25 12:24:28 +0800

    .

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -3,5 +3,5 @@
 
 #mkdir data
 chown -R 1000.1000 data
-docker run -d -p 8080:8080 -p 50000:50000 -u 0 -v `pwd`/data:/var/jenkins_home gsy36559067/jenkins:2.92
+docker run -d -p 8080:8080 -p 50000:50000 -u root -v `pwd`/data:/var/jenkins_home gsy36559067/jenkins:2.92
 #docker run -d -p 8080:8080 -p 50000:50000 -v `pwd`/data:/var/jenkins_home jenkins/jenkins:2.92-alpine
